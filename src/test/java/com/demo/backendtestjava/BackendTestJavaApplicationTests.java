86dced4d97c66ebac68a805181cd27f0443133af
Pedro Henrique <pedro2266809ph@gmail.com>
2024-03-07 18:24:40 -0300

    initial commit

---
+++ src/test/java/com/demo/backendtestjava/BackendTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.backendtestjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
